79f3cad35be0d3d56e3f71121fe22c20644d9d6c
Anany-Pandey <110780191+Anany-Pandey@users.noreply.github.com>
2024-05-04 00:39:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const http= require('http');
+const http=require('http');
 const fs= require('fs').promises;
 const ws= require("ws");
+const security=require("./backend/security.js");
+const bcrypt = require("bcrypt");
 const port=3000;
+
+async function dlt(){
+    await fs.writeFile("users.txt","{}");
+}
+dlt();
+delete dlt;
 
 async function load(s,encoding){
 if(!encoding)
@@ -11,7 +19,6 @@ return fs.readFile(s,encoding);
 }
 function parse(s){
 s=s.substring(1,s.length);
-s.split("/");
 return s.split("/");
 }
 function contains(obj,key){
@@ -21,37 +28,39 @@ function contains(obj,key){
     }
     return false;
 }
-async function createAccount(usr,pass){
-    let obj=JSON.parse(await load("users.txt","utf-8"));
-    if(!contains(obj,usr)){
-        obj[usr]={pass:pass,data:{msgs:{}}};
-        await fs.writeFile("users.txt", JSON.stringify(obj, null, 2));
-    }
-    else{
+
+async function createAccount(username, password) {
+    password=await bcrypt.hash(password,10);
+    try {
+        const users = JSON.parse(await load("users.txt","utf-8"));
+        if (!users[username]) {
+            users[username] = { pass: password, data: { msgs: {} } };
+            await fs.writeFile("users.txt", JSON.stringify(users, null, 2));
+            return true; // Account created successfully
+        } else {
+            return false; // Username already exists
+        }
+    } catch (error) {
+        console.error("Error creating account:", error);
         return false;
     }
 }
-async function authenticate(usr,pass){
-    let obj=JSON.parse(await load("users.txt","utf-8"));
-    try{
-        if(obj[usr].pass==pass){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    catch{
+
+async function authenticate(username, password) {
+    try {
+        const users = JSON.parse(await load("users.txt","utf-8"));
+        return users[username] && await bcrypt.compare(password,users[username].pass);
+    } catch (error) {
+        console.error("Error authenticating:", error);
         return false;
     }
 }
-function getmsgs(usr){
-    let obj=JSON.parse(await load("users.txt","utf-8"));
+async function getmsgs(usr){
+    let obj = JSON.parse(await load("users.txt","utf-8"));
     return obj[usr].data.msgs;
 }
 const server=http.createServer(async (req,res)=>{
 let request=parse(req.url);
-console.log(req.url);
 if(req.url=="/"){
     res.writeHead(200,{"content-type":"text/html"});
     res.write(await load("frontend/login.html","utf-8"));
@@ -62,31 +71,43 @@ else if(req.url=="/signup.html"){
     res.write(await load("frontend/signup.html","utf-8"));
     res.end();
 }
-else if(req.url=="/auth.jpg"){
-    res.writeHead(200,{"content-type":"image/jpg"});
-    res.write(await load("frontend/auth.jpg"));
-    res.end();
-}
 else if(request[0]=="Create"){
     if(contains(JSON.parse(await load("users.txt","utf-8")),request[1])){
         res.writeHead(200,{"Content-Type":"text/plain"});
         res.write("Username is already taken");
+        res.end();
+    }
+    else if(request[1]=="" || request[2]==""){
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.write("Password OR Username Cannot Be Empty.".toUpperCase());
+        res.end();
     }
     else{
-        createAccount(request[1],,request[2]);
+        createAccount(request[1],request[2]);
         res.writeHead(200,{"Content-Type":"text/plain"});
         res.write("Created");
+        res.end();
     }
 }
 else if(request.length>=3 && await authenticate(request[0],request[1])){
-    if(request[3]==""){
+    if(request[2]==""){
         res.writeHead(200,{"content-type":"text/html"});
         res.write(await load("frontend/main_menu.html"));
         res.end();
     }
-    else if(request[3]=="msgs"){
+    else if(request[2]=="msgs"){
         res.writeHead(200,{"content-type":"text/html"});
         res.write(await load("frontend/msg.html"));
+        res.end();
+    }
+    else if(request[2]=="send.svg"){
+        res.writeHead(200,{"content-type":"image/svg+xml"});
+        res.write(await load("send.svg"));
+        res.end();
+    }
+    else if(request[2]=="verify"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.write("Ok");
         res.end();
     }
     else{
@@ -95,10 +116,18 @@ else if(request.length>=3 && await authenticate(request[0],request[1])){
         res.end();
     }
 }
-else if(request.length>=3 && !await authenticate(request[0],request[1])){
-    res.writeHead(200,{"content-type":"text/html"});
-    res.write(await load("frontend/redirect.html","utf-8"));
-    res.end();
+else if(request.length>=3 && !(await authenticate(request[0],request[1]))){
+    if(request[2]=="verify"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.write("Either Password Or Username is Wrong");
+        res.end();
+        return;
+    }
+    else{
+        res.writeHead(200,{"content-type":"text/html"});
+        res.write(await load("frontend/redirect.html","utf-8"));
+        res.end();
+        }
 }
 else{
     res.writeHead(404,{"content-type":"text/plain"});
@@ -107,25 +136,45 @@ else{
 }
 }
 );
-
-const socket=new ws.Server({Server:server});
-socket.on("connection",async (user)=>{
-    let usr;
-    user.on("message"),(data)=>{
-        data=data.split('/');
-        if(!authenticate(data[0],data[1])){
-            user.close();
+const socket=new ws.Server({server:server});
+const UserConnected={};
+socket.on("connection",async(user,req)=>{
+    let request=parse(req.url);
+    if(!await authenticate(request[0],request[1])){user.close();return;}
+    let obj=JSON.parse((await load("users.txt","utf-8")));
+    let usr=request[0];
+    user.send(JSON.stringify(obj[usr]["data"]["msgs"]));
+    UserConnected[usr]=user;
+    user.on("message",async (data)=>{
+        data=data.toString();
+        data=data.split("%#");
+        data[0]=data[0].replace(/ /g, "%20");
+        data[1]=security.sanitize(data[0]);
+        let obj=JSON.parse((await load("users.txt","utf-8")));
+        if(data.length==1){
+            user.send(""+contains(obj,data[0]));
             return;
         }
-        usr=[data[0]];
-        socket.send(getmsgs(usr));
-    };
-    if(usr==undefined)return;
-    user.on("message",(data)=>{
-        let obj=JSON.parse(await load("users.txt","utf-8"));
-        let format="<li class=\"a\">data<li>";
-        data=data.split("%#");
-        obj[usr][data][data[0]]+=format.replace("data",data[1]);
+        let format='<p class="a">data</p>';
+        if(obj[usr]["data"]["msgs"][data[0]]==undefined){
+            obj[usr]["data"]["msgs"][data[0]]=format.replace("data",data[1]);
+        }
+        else{
+            obj[usr]["data"]["msgs"][data[0]]+=format.replace("data",data[1]);
+        }
+        if(obj[data[0]]["data"]["msgs"][usr]==undefined){
+            obj[data[0]]["data"]["msgs"][usr]='<p class="b">data</p>'.replace("data",data[1]);
+        }
+        else{
+            obj[data[0]]["data"]["msgs"][usr]+='<p class="b">data</p>'.replace("data",data[1]);
+        }
+        user.send(JSON.stringify(obj[usr]["data"]["msgs"]));
+        UserConnected[data[0]]?.send(JSON.stringify(obj[data[0]]["data"]["msgs"]))
+        fs.writeFile("users.txt",JSON.stringify(obj,null,2));
     });
+    user.on("close",()=>{
+        delete UserConnected[user];
+    });
+}
 );
 server.listen(port);
